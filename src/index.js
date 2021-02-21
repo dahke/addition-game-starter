244a19f5bef2c773535fb18728a450f15a18e1ac
dahke <dahhke@gmail.com>
2021-02-22 02:56:25 +0900

    컨트랙에 Klay 송금

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const config = {
   rpcURL: 'https://api.baobab.klaytn.net:8651'
 }
 const cav = new Caver(config.rpcURL);
+const agContract = new cav.klay.Contract(DEPLOYED_ABI, DEPLOYED_ADDRESS);
 const App = {
   auth: {
     accessType: 'keystore',
@@ -72,19 +73,47 @@ const App = {
   },
 
   deposit: async function () {
-
+    const walletInstance = this.getWallet();
+    if (walletInstance) {
+      if (await this.callOwner() !== walletInstance.address) return;
+      else {
+        var amount = $('#amount').val();
+        if (amount) {
+          agContract.methods.deposit().send({
+            from: walletInstance.address,
+            gas: '250000',
+            value: cav.utils.toPeb(amount, "KLAY")
+          })
+          .once('transactionHash', (txHash) => {
+            console.log(`txHash: ${txHash}`);
+          })
+          .once('receipt', (receipt) => {
+            console.log(`(#${receipt.blockNumber})`, receipt);
+            alert(amount + " KLAY를 컨트랙에 송금했습니다.");
+            location.reload();
+          })
+          .once('error', (error) => {
+            alert(error.message);
+          });
+        }
+        return;
+      }
+    }
   },
 
   callOwner: async function () {
-
+    // await 키워드를 사용하여 비동기로 값을 받음
+    return await agContract.methods.owner().call();
   },
 
   callContractBalance: async function () {
-
+    return await agContract.methods.getBalance().call();
   },
 
   getWallet: function () {
-
+    if (cav.klay.accounts.wallet.length) {
+      return cav.klay.accounts.wallet[0];
+    }
   },
 
   checkValidKeystore: function (keystore) {
@@ -119,6 +148,12 @@ const App = {
     $('#login').hide();
     $('#logout').show();
     $('#address').append('<br>' + '<p>' + '내 계정 주소: ' + walletInstance.address + '</p>');
+    $('#contractBalance')
+    .append('<p>' + '이벤트 잔액: ' + cav.utils.fromPeb(await this.callContractBalance(), "KLAY") + ' KLAY' + '</p>');
+    
+    if (await this.callOwner() === walletInstance.address) {
+      $('#owner').show();
+    } 
   },
 
   removeWallet: function () {
--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ const PrivateKeyConnector = require('connect-privkey-to-provider')
 const NETWORK_ID = '1001'   // baobab 고유의 network id
 const GASLIMIT = '20000000' // 배포하는 데에 들어가는 gas 한도
 const URL = 'https://api.baobab.klaytn.net:8651'    // baobab test net
-const PRIVATE_KEY = '0x769e1d7c7f46ea3d69f80047b134b5aa39432aecefb9f26dafba0fd6ca802b9d'
+const PRIVATE_KEY = '0x0f5f39181ac9e57e0cba994bd106ce74304fc0992387305f516df5a3b7d5f585'
 
 module.exports = {
     networks: {
